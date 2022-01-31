7f78a75a0101f7eb6f2ea1a2af6132e83a9b03f6
Evgeny Shchukin <shchukin.work@gmail.com>
2022-02-01 00:14:06 +0300

    update lesson08.py, commit from pycharm

---
+++ lesson08.py
@@ -0,0 +1,150 @@
+# Задача 1
+
+from datetime import date
+
+
+class Data:
+    def __init__(self, data):
+        self.data = data.split('-')
+
+    @classmethod
+    def type(cls, data):
+        try:
+            day, month, year = [int(i) for i in data.split('-')]
+            return f"{type(day), day}\n{type(month), month}\n{type(year), year}"
+        except ValueError:
+            return 'Указана неправильная дата!'
+
+    @staticmethod
+    def valid(data):
+        try:
+            day, month, year = data.split('-')
+            date(int(year), int(month), int(day))
+            return 'Есть такая дата!'
+        except ValueError:
+            return 'Указана неправильная дата!'
+
+
+print(Data.valid('25-02-2015'))
+print(Data.type('25-02'))
+
+# Задача 2
+
+
+class OwnError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+def div():
+    try:
+        user_num_1 = int(input('Введите числитель: '))
+        user_num_2 = int(input('Введите знаменатель: '))
+        if user_num_2 == 0:
+            raise OwnError("Делить на ноль нельзя!")
+        else:
+            res = user_num_1 / user_num_2
+            return res
+    except ValueError:
+        return "Вы ввели не число"
+    except OwnError as err:
+        return err
+
+
+print(div())
+
+# Задача 3
+
+class MyError(Exception):
+    def __init__(self):
+        pass
+
+
+class TypeCheck:
+    def __init__(self):
+        self.my_list = []
+
+    def check_value(self):
+        global user_val
+        while True:
+            try:
+                try:
+                    user_val = int(input('Введите числа: '))
+                    ex = input(f'Всё отлично, добавляем "{user_val}" в список. Хотите продолжить? y/n: ').lower()
+                    self.my_list.append(user_val)
+                    if ex == 'n':
+                        print(self.my_list)
+                        break
+                except ValueError:
+                    raise MyError
+            except MyError:
+                ex = input(f"Вы ввели не число! Хотите продолжить? y/n: ").lower()
+                if ex == 'n':
+                    print(self.my_list)
+                    break
+                else:
+                    self.check_value()
+
+
+a = TypeCheck()
+a.check_value()
+
+# Задача 4-6
+
+class OfficeEquipment:
+
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.items = {'Модель устройства': self.name, 'Цена за ед': self.price, 'Количество': self.quantity}
+
+    def income(self):
+        try:
+            name = input(f'Введите наименование: ')
+            price = int(input(f'Введите цену за ед: '))
+            quantity = int(input(f'Введите количество: '))
+            item = {'Модель устройства': name, 'Цена за ед': price, 'Количество': quantity}
+            self.items.update(item)
+            print(self.items)
+        except ValueError:
+            print('Недопустимое значение!')
+
+
+class Printer(OfficeEquipment):
+    pass
+
+
+class Scanner(OfficeEquipment):
+    pass
+
+
+class Xerox(OfficeEquipment):
+    pass
+
+
+p = Printer('Hp', 2, 300)
+s = Scanner('Canon', 54000, 10)
+x = Xerox('Xerox', 7000, 15)
+p.income()
+s.income()
+x.income()
+
+# Задача 7
+
+class ComplexNumber:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __add__(self, other):
+        return f'Сумма равна: {self.a + other.a} + {self.b + other.b} * i'
+
+    def __mul__(self, other):
+        return f'Произведение равно: {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+
+
+c_1 = ComplexNumber(4, -8)
+c_2 = ComplexNumber(6, 11)
+print(c_1 + c_2)
+print(c_1 * c_2)
